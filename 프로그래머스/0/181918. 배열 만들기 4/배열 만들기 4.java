fb60715cb6b4d82fac5ed98d38fd0cd7a14e73ff
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-09-29 19:43:45 +0900

    [level 0] Title: 배열 만들기 4, Time: 31.88 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        int i = 0;
+        
+        while (true) {
+            if (i == arr.length) {
+                break;
+            }
+            
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+            } else {
+                stk.remove(stk.size() - 1);
+            }
+        }
+        
+        return stk.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
