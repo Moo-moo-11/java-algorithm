10d5db07632b4d03953181019ecf19425527842c
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-10-05 13:56:10 +0900

    [level 0] Title: rny_string, Time: 0.06 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String rny_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (char ch : rny_string.toCharArray()) {
+            if (ch == 'm') {
+                sb.append("rn");
+            } else {
+                sb.append(ch);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
