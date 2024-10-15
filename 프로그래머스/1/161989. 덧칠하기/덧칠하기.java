bdd7e387a2be5b9de39ffa3432b83bcfa4a79113
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-10-15 13:46:45 +0900

    [level 1] Title: 덧칠하기, Time: 2.28 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        int start = section[0];
+        answer++;
+        
+        for (int sec : section) {
+            if (start + m > sec) {
+                continue;
+            }
+            start = sec;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
