c027a1fc5b6ae2116c7c3c1790cba8b1582bfc3f
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-12-26 16:04:54 +0900

    Time: 1 ms (61.16%) | Memory: 42.2 MB (10.63%) - LeetSync

--- leetcode/782-jewels-and-stones/jewels-and-stones.java
+++ leetcode/782-jewels-and-stones/jewels-and-stones.java
@@ -2,19 +2,15 @@ class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int answer = 0;
 
-        Map<Character, Integer> maps = new HashMap<>();
-
-        for (char s: stones.toCharArray()){
-            if (maps.containsKey(s)) {
-                maps.put(s, maps.get(s) + 1);
-            } else {
-                maps.put(s, 1);
-            }
-        }
+        Set<Character> set = new HashSet<>();
 
         for (char j: jewels.toCharArray()) {
-            if (maps.containsKey(j)) {
-                answer += maps.get(j);
+            set.add(j);
+        }
+
+        for (char s: stones.toCharArray()){
+            if (set.contains(s)) {
+                answer++;
             }
         }
         
